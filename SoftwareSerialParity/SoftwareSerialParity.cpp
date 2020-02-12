08844d292f03afcf548c2d5506e16580c590c86a
robert-hh <robert@hammelrath.com>
2020-02-12 15:49:07 +0100

    SoftwareSerialParity.cpp
    Fine-tune the timing if method write, such that it works well for baud rates up to 19200 Baud @ 1MHZ, and up
    to 230400 baud @ 16 MHz. The code does definitely not look better now, but it works better.

--- SoftwareSerialParity/SoftwareSerialParity.cpp
+++ SoftwareSerialParity/SoftwareSerialParity.cpp
@@ -418,9 +418,7 @@ int SoftwareSerialParity::available()
     return 0;
 
   return (_receive_buffer_tail + _SS_MAX_RX_BUFF - _receive_buffer_head) % _SS_MAX_RX_BUFF;
-}
-
-size_t SoftwareSerialParity::write(uint8_t b)
+}size_t SoftwareSerialParity::write(uint8_t b)
 {
   if (_tx_delay == 0) {
     setWriteError();
@@ -437,10 +435,24 @@ size_t SoftwareSerialParity::write(uint8_t b)
   uint8_t oldSREG = SREG;
   bool inv = _inverse_logic;
   uint16_t delay = _tx_delay;
-
+  if (F_CPU == 1000000L) { // compensate for slow CPU clock
+    delay -= 1;
+  }  
   if (inv)
     b = ~b;
 
+  Cparity = 0;
+  // Calculate parity
+  for (uint8_t i = 8, cb = b; i > 0; --i, cb >>= 1)
+  {
+    Cparity += (cb & 1);
+  }
+  if (Tparity != EVEN) 
+  {
+    Cparity = ~ Cparity;
+  }
+  
+  uint8_t i = 8; // moveing that out of the for loop shifts the first bit upfront
   cli();  // turn off interrupts for a clean txmit
 
   // Write the start bit
@@ -450,38 +462,32 @@ size_t SoftwareSerialParity::write(uint8_t b)
     *reg &= inv_mask;
 
   tunedDelay(delay);
-  Cparity = 0;
+
   // Write each of the 8 bits
-  for (uint8_t i = 8; i > 0; --i)
+  for (; i > 0; --i)
   {
-    if (b & 1){ // choose bit
+    if (b & 1) // choose bit
       *reg |= reg_mask; // send 1
-	  Cparity++;
-	}
-    else{
+    else 
       *reg &= inv_mask; // send 0
-	}
+
     tunedDelay(delay);
     b >>= 1;
   }
 
-    if (Tparity == ODD){ //odd parity
-		if (Cparity & 1) // choose bit
-		  *reg &= inv_mask; // send 0
-		else
-		  *reg |= reg_mask; // send 1
+  // Send parity or a stop bit
+  // The EVEN/ODD selection is adjusted outside the critical timing
+  // The Parity bit is slightly delayed. If that is a problem.
+  // remove the conditional from next line. In case of partity NONE
+  // a seconds stop bit will be sent then.
+  if (Tparity != NONE) {
+    if (Cparity & 1) // choose bit
+      *reg |= reg_mask; // send 1
+    else
+      *reg &= inv_mask; // send 0
 
-		tunedDelay(delay);
-	}
-
-    if (Tparity == EVEN){ //even parity
-		if (Cparity & 1) // choose bit
-		  *reg |= reg_mask; // send 1
-		else
-		  *reg &= inv_mask; // send 0
-
-		tunedDelay(delay);
-	}
+    tunedDelay(delay);
+  }
 
 
 	// restore pin to natural state
